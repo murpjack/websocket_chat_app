7711f7ddf88ebe2e03c3952ca30d786e5278a7de
Jack <jackmurphy756@gmail.com>
2020-08-23 20:34:15 +0100

    use css in js to style App

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { colours } from "./styles/variables";
 
-import Chat from './components/Chat'
+import Chat from './components/Chat';
+
+const StyledApp = styled.div`
+  background-image: url("./images/background.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 100vh;
+  position: fixed;
+  text-align: center;
+  width: 100%;
+`;
+
+const StyledHeader = styled.div`
+  background-color: ${colours.bgPrimary};
+  height: 75px;
+  margin: 0 0 30px;
+  text-align: center;
+  width: 100%;
+`;
+
+const StyledTitle = styled.h1`
+  color: #fff;
+  line-height: 75px;
+  margin: 0;
+  text-align: center;
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Chatty</h1>
-      </header>
+    <StyledApp className="App">
+      <StyledHeader>
+        <StyledTitle>Chatty</StyledTitle>
+      </StyledHeader>
       <Chat />
-    </div>
+    </StyledApp>
   );
 }
+
+
 export default App
---
+++ client/src/styles/variables.js
@@ -0,0 +1,4 @@
+export const colours = {
+  bgPrimary: "#06d6a0",
+  fontPrimary: "#073b4c",
+}
