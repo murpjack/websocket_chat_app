8a3cd50e8c87afd463ab8c006bd8ffac0d0a3c18
Jack <jackmurphy756@gmail.com>
2020-08-23 21:33:22 +0100

    update server watching message, remove server log data

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 3030 });
 
-console.log("Websocket connection starting...");
+console.log("Websocket connection watching for updates.");
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log("data", data);
         client.send(data);
       }
     });
